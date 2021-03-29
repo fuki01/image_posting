4b664316dd93c95af58e7f578acadc9339b5b6ee
fuki <fuki01@users.noreply.github.com>
2021-03-29 20:39:01 +0900

    サンプルデータの更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ CSV.foreach('db/csv/sample_seed_context.csv', headers: true) do |row|
     user_id:     row['user_id'],
     body:        row['body'],
     category:    row['category'],
-    context_img: open("./app/assets/sample_images/ファイル #{rand(1..144)}.jpeg")
+    context_img: open("./app/assets/sample_images/ファイル #{rand(1..59)}.jpeg")
   )
 end
