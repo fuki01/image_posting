6d607679b3b097cd4f022eeffe0787a40d2ae6a5
fuki <fuki01@users.noreply.github.com>
2021-03-31 17:04:23 +0900

    contextのテストを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -30,8 +30,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+Rails.logger = Logger.new(STDOUT)
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include LoginMacros
+  config.include ContextMacros
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -62,4 +65,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
 end
---
+++ spec/requests/contexts_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+RSpec.feature "Contexts", type: :feature do
+  before do
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
+  it 'Contextが投稿ができること' do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_on "ログイン"
+    login user
+    expect{
+      click_on   "投稿する！"
+      context_create
+    }.to change(user.context, :count).by(1)
+  end
+
+  it 'Contextが削除できること' do
+    user = FactoryBot.create(:user)
+    expect{
+      FactoryBot.create(:context, user: user)
+      visit root_path
+      click_on "ログイン"
+      login user
+      click_on "show-img"
+      click_on "削除"
+    }.to change(user.context, :count).by(0)
+  end
+
+  it 'Contextが編集できること' do
+    user = FactoryBot.create(:user)
+    context = FactoryBot.create(:context, user: user)
+    visit root_path
+    click_on "ログイン"
+    login user
+    click_on "show-img"
+    click_on "編集"
+    context_edit
+    expect(user.context).not_to eq context
+  end
+end
